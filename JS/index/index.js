983b747a0e37c755f17c80d8340bc465bee320d7
Christian <chris@polarchris.com>
2025-04-09 12:37:43 +0200

    completed task 2 toggle-functionality-answer-button

--- JS/index/index.js
+++ JS/index/index.js
@@ -5,4 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
   bookmarkIcon.addEventListener("click", () => {
     bookmarkIcon.classList.toggle("active");
   });
+
+  const answerButton = document.querySelector('[data-js="answer-button1"]');
+  const answer1 = document.querySelector(".answer1");
+
+  answerButton.addEventListener("click", () => {
+    answer1.classList.toggle("hidden");
+
+    if (answer1.classList.contains("hidden")) {
+      answerButton.textContent = "show answer";
+    } else {
+      answerButton.textContent = "hide answer";
+    }
+  });
 });
